364cacb7aeaea6d1556a70e4e43bc10fc753ae2c
Chithrive <thechiomaemelda@gmail.com>
2023-02-16 05:34:10 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
  * Return: has to return 0 if it works
  */
 int main(void)
-{puts("Programming is like building a multilingual puzzle");
+{
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
