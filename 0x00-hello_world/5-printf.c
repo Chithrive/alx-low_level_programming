81d97c6a5441d966dec125408a31636005143220
Chithrive <thechiomaemelda@gmail.com>
2023-02-16 05:53:33 -0800

    errfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
  * main - prints a message
  * Return: should return 0 if no errors
  */
-int main()
+int main(void)
 {
-printf("This is a piece of art, but the outcome is not just a product of chance.\n");
-return 0;
+printf("This is a piece of art, but the outcome is no
+t just a product of chance.\n");
+return (0);
 }
 
