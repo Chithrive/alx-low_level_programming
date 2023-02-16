52da6e848382e8e9e290099c11ddcbf6d5d54701
Chithrive <thechiomaemelda@gmail.com>
2023-02-16 06:40:08 -0800

    added void

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - prints a message
  * Return: if no erro return 0
  */
-int main()
+int main(void)
 {
 printf("This is a piece of art, but the outcome is not just a product of chance.\n");
 return (0);
