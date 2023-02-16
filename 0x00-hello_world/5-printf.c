af93cdc2633a4cda04c29c2288375a39163c6da7
Chithrive <thechiomaemelda@gmail.com>
2023-02-16 06:33:10 -0800

    rewrite

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * main - prints a message
- * Return: should return 0 if no errors
+ * Return: if no erro return 0
  */
-int main(void)
+int main()
 {
-printf("This is a piece of art, but the outcome is no
-t just a product of chance.\n");
+printf("This is a piece of art, but the outcome is not just a product of chance.\n");
 return (0);
 }
-
