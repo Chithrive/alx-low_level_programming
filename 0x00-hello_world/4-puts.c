109e7d0356c3700d97d944661c21e3981b8ccd31
Chithrive <thechiomaemelda@gmail.com>
2023-02-16 05:24:57 -0800

    betty fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
+/**
+ * main-prints a message
+ * Return: has to return 0 if it works
+ */
+int main(void) 
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
