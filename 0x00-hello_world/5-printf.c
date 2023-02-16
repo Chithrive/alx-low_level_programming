44e71f375e89578e58019975867635fc334bf074
Chithrive <thechiomaemelda@gmail.com>
2023-02-16 05:50:54 -0800

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - prints a message
+ * Return: should return 0 if no errors
+ */
+int main()
+{
+printf("This is a piece of art, but the outcome is not just a product of chance.\n");
+return 0;
+}
+
