2394477f031ca2a027f1a5a9b80714d0879c4954
Chithrive <thechiomaemelda@gmail.com>
2023-02-17 07:04:38 -0800

    all tasks once

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 9 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
