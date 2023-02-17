496b90c4eb104ff47f47722158f22f8a767191e2
Chithrive <thechiomaemelda@gmail.com>
2023-02-17 06:19:18 -0800

    small and caps letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar('a' + i);
+}
+for (i = 0; i < 26; i++)
+{
+putchar('A' + i);
+}
+putchar('\n');
+return (0);
+}
